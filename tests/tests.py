ce29897d23798e2e355d0d9a6a57043d8fb8f361
michlee1337 <michlee1337@gmail.com>
2020-04-13 22:53:02 +0800

    remove from setup

--- tests/tests.py
+++ tests/tests.py
@@ -3,35 +3,32 @@ import requests
 import psycopg2
 import unittest
 
+connection = psycopg2.connect(user="cs162_user",
+                                  password="cs162_password",
+                                  host="localhost",
+                                  port="5432",
+                                  database="cs162")
+cursor = connection.cursor()
+
 class TestCase(unittest.TestCase):
-    def setUp(self):
-        connection = psycopg2.connect(user="cs162_user",
-                                          password="cs162_password",
-                                          host="localhost",
-                                          port="5432",
-                                          database="cs162")
-        self.connection = connection.cursor()
-        self.endpoint = 'http://127.0.0.1:5000/add'
-
-
     def test_connection(self):
-        response = requests.post(self.endpoint ,data={'expression':'2+2'})
+        response = requests.post('http://127.0.0.1:5000/add',data={'expression':'2+2'})
         assert response.status_code == 200
 
     def test_post(self):
-        self.connection.execute("SELECT * FROM Expression WHERE text='2+2' LIMIT 1")
-        es = self.connection.fetchall()
+        cursor.execute("SELECT * FROM Expression WHERE text='2+2' LIMIT 1")
+        es = cursor.fetchall()
         assert es is not None
         assert es[0] is not None
         assert es[0][2] == 4
 
     def test_err(self):
-        response = requests.post(self.endpoint, data={'expression':'2/0'})
+        response = requests.post('http://127.0.0.1:5000/add', data={'expression':'2/0'})
         assert response.status_code == 500
 
     def test_db(self):
-        self.connection.execute("SELECT * FROM Expression ORDER BY id DESC LIMIT 1")
-        es = self.connection.fetchall()
+        cursor.execute("SELECT * FROM Expression ORDER BY id DESC LIMIT 1")
+        es = cursor.fetchall()
         assert es is not None
         assert es[0] is not None
         assert es[0][1] == '2+2'
