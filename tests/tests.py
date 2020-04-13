f35b9b16b9959059fa686718272894bd2d3cb62c
michlee1337 <michlee1337@gmail.com>
2020-04-13 23:10:24 +0800

    setup

--- tests/tests.py
+++ tests/tests.py
@@ -2,21 +2,22 @@ import requests
 import psycopg2
 import unittest
 
-connection = psycopg2.connect(user="cs162_user",
-                                  password="cs162_password",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="cs162")
-cursor = connection.cursor()
-
 class TestCase(unittest.TestCase):
+    def setUp(self):
+        connection = psycopg2.connect(user="cs162_user",
+                                          password="cs162_password",
+                                          host="127.0.0.1",
+                                          port="5432",
+                                          database="cs162")
+        self.cursor = connection.cursor()
+
     def test_connection(self):
         response = requests.post('http://127.0.0.1:5000/add',data={'expression':'2+2'})
         assert response.status_code == 200
 
     def test_post(self):
-        cursor.execute("SELECT * FROM Expression WHERE text='2+2' LIMIT 1")
-        es = cursor.fetchall()
+        self.cursor.execute("SELECT * FROM Expression WHERE text='2+2' LIMIT 1")
+        es = self.cursor.fetchall()
         assert es is not None
         assert es[0] is not None
         assert es[0][2] == 4
@@ -26,8 +27,8 @@ class TestCase(unittest.TestCase):
         assert response.status_code == 500
 
     def test_db(self):
-        cursor.execute("SELECT * FROM Expression ORDER BY id DESC LIMIT 1")
-        es = cursor.fetchall()
+        self.cursor.execute("SELECT * FROM Expression ORDER BY id DESC LIMIT 1")
+        es = self.cursor.fetchall()
         assert es is not None
         assert es[0] is not None
         assert es[0][1] == '2+2'
